1d63af3658fbced2cfeebbf28fd8b1d0f15b119f
RenardFake <rmacorol18@gmail.com>
2023-05-15 16:02:29 +0800

    Update CompareHeight.java

--- Number1/project1/src/CompareHeight.java
+++ Number1/project1/src/CompareHeight.java
@@ -2,7 +2,7 @@ package Number1.project1.src;
 
 
 public class CompareHeight {
-    CompareHeight(){
-        System.out.println("Heigh Comparison of 3 persons");
+    public static void compare(int person1,int person2, int person3) {
+        
     }
 }
